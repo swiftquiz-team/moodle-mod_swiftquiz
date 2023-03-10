f78e65c42d8e9cf408af44eed081fd4dfb56fe21
Lorain <743804605@qq.com>
2023-03-10 18:03:57 +0800

    chore: update version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022021937; // The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2023020501; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2018051700; // Moodle 3.5 (or above)
 $plugin->cron = 0; // Period in seconds for cron to run.
 $plugin->component = 'mod_swiftquiz';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0 (Build: 2019073000)';
+$plugin->release = '0.1.1 (Build: 2023020501)';
